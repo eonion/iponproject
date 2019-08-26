05de5f2ed728e4110cb8a801cea55f3919f38252
eonion <neteonion@gmail.com>
2019-08-26 23:48:31 +0800

    Initial commit

---
+++ src/main/java/com/unipec/Hello.java
@@ -0,0 +1,9 @@
+package com.unipec;
+
+public class Hello {
+    public static void main(String[] args) {
+        //System.out.println("Hello Word");
+       Person p = new Person();
+       p.hello();
+    }
+}
